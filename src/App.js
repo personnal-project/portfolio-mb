54ae04aa7fa7c8e14ef122076da11fb8292b85fb
KevinMB22 <96531261+2C-Conseil@users.noreply.github.com>
2022-04-07 11:21:02 +0200

    Update portofolio

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       setLoading(true)
       setTimeout(() => {
           setLoading(false)
-      }, 2000) 
+      }, 2200) 
   },[])
 
   
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,8 +9,12 @@ function About(){
             <section className="About-pc">
                 <div className="about-id-pc">3D Face is coming</div>
                 <div className="about-intro-pc">
-                    <h1>Je suis Mboumba Kevin, la programmation sinon l'informatique en general a toujours ete une source d'inspiration pour moi depuis le plus jeune age</h1>
+                    <h1>Je suis Mboumba Kevin, l'informatique en général a toujours été une source d'inspiration pour moi depuis le plus jeune age</h1>
                 </div>
+                <div className="about-cir">
+                <div className="about-circle-up"></div>
+                </div>
+               
             </section>
         </div>
     )
