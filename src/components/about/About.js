d96849f633bb20cdad7a5cf1d9cb647d0f1f7eff
KevinMB22 <96531261+2C-Conseil@users.noreply.github.com>
2022-04-09 18:53:21 +0200

    Update about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 import './about.scss'
 
 
@@ -9,8 +10,14 @@ function About(){
             <section className="About-pc">
                 <div className="about-id-pc">3D Face is coming</div>
                 <div className="about-intro-pc">
-                    <h1>Je suis Développeur Web</h1>
+                    <h1>Je suis Développeur <span className="typing"> <Typewriter
+                        onInit={(typewriter) =>{
+                        typewriter.typeString("Web").pauseFor(1000).deleteAll().typeString("Mobile").pauseFor(1000).deleteAll().typeString("IA").pauseFor(1000).deleteAll().typeString("Web").pauseFor(1000).deleteAll().typeString("Mobile").pauseFor(1000).deleteAll().typeString("IA").pauseFor(1000).deleteAll().typeString("Web").start();
+                        }}
+                        /></span> </h1>
+
                     <h1>Mon expertise comprend le développement et la maintenance d'applications et de sites Web responsive. Je suis spécialisé dans le développement Web et Application</h1>
+
                 </div>
                 <div className="about-cir">
                 <div className="about-circle-up"></div>
